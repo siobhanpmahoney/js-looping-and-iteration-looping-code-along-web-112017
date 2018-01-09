014d50e79e44f851867a7afbfa62370c1d263c8b
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-01-09 12:07:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`)
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let i = 0;
+  function maybeTrue () {
+    return Math.random() >= 0.5;
+  }
+  while (maybeTrue()) {
+    i++
+  }
+  return `You got ${i} tails in a row!`
+}
